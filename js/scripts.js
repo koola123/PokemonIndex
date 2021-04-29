32f9e548831680d1ce8429ecbad3657e99257857
koola123 <82808273+koola123@users.noreply.github.com>
2021-04-29 19:01:07 +0200

    Connected local pokedex--js-app folder to GitHub

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,2 @@
-let pokemonList = [{name: "Bulbasaur", height: 0.7, type: ['grass', 'poison']}, 
-                   {name: "Nidoking", height: 1.4, type: ['ground', 'poison']}, 
-                   {name: "Onix", height: 8.8, type: ['rock', 'ground']}];
+// Created three Objects as Pokemons in an Array
+let pokemonList = [{name: "Bulbasaur", height: 0.7, type: ['grass', 'poison']}, {name: "Nidoking", height: 1.4, type: ['ground', 'poison']}, {name: "Onix", height: 8.8, type: ['rock', 'ground']}];
