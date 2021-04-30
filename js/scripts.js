219a09837b0c23dbd148a3dcc6717d53330a3d1b
koola123 <82808273+koola123@users.noreply.github.com>
2021-04-30 23:27:19 +0200

    Update scripts.js
    Added a  conditional to the loop for  printing the biggest pokemon by height.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-// Defined three objects as pokemons in an Array:
+// Defined three objects as pokemon in an Array:
 let pokemonList = [
   {name: "Bulbasaur", height: 0.7, type: ['grass', 'poison']},
   {name: "Nidoking", height: 1.4, type: ['ground', 'poison']},
   {name: "Onix", height: 8.8, type: ['rock', 'ground']}
   ];
 
-// A loop that iterates over each item in the Array:
+/* Created a loop that iterates over each item in the Array, added text for
+sizing the height of each pokemon and put code into loop for printing
+the biggest pokemon via definition of height */
 for (let i = 0; i <pokemonList.length; i++) {
   if (pokemonList[i].height <1.0 && pokemonList[i].height >0.5) {
     console.log(pokemonList[i].name);
@@ -15,6 +17,6 @@ for (let i = 0; i <pokemonList.length; i++) {
     document.write(pokemonList[1].name + ' (height: 1.4) ');
   } else {
     console.log(pokemonList[i].name);
-    document.write(pokemonList[2].name + ' (height: 8.8) ');
+    document.write(pokemonList[2].name + ' (height: 8.8) ' + '- Wow, that\'s big! ');
   }
 }
