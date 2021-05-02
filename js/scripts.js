01cef78777df107ec7f91d14e95bb989e28053ab
koola123 <82808273+koola123@users.noreply.github.com>
2021-05-02 21:57:19 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Defined three objects as pokemon in an Array:
+// Defined three objects as pokemon in an Array
 
 let pokemonList = [
   {name: "Bulbasaur", height: 0.7, type: ['grass', 'poison']},
@@ -6,13 +6,17 @@ let pokemonList = [
   {name: "Onix", height: 8.8, type: ['rock', 'ground']}
   ];
 
-/* Created a loop that iterates over each item in the Array, added text for
+// Loop iterates over each item and prints biggest pokemon to the console
+
 sizing the height of each pokemon and put code into loop for printing
 the biggest pokemon via definition of height */
 
-for (let i = 0; i <pokemonList.length; i++) {
-  if (pokemonList[i].height >0.5) {
-   console.log(pokemonList[i]);
-
-document.write("<p>" + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big! ' + "</p>"); }
-}
+for (let i=0; i<pokemonList.length; i++) {
+    if (pokemonList[i].height <8.8) {
+      document.write("<p>" + pokemonList[i].name + ' (height: ' + pokemonList[i].height +') ' + "</p>");
+      console.log(pokemonList[i].name + " (height: 0.7");
+    }else {
+      document.write("<p>" + pokemonList[i].name + " (height: 8.8)" + " - Wow, that's big!" + "</p>");
+      console.log(pokemonList[i].name + " (height: 8.8)" + " - Wow, that's big!");
+    }
+  }
