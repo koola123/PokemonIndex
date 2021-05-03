71862d5323b3bcdd19545a1cdbe780296d76b301
koola123 <82808273+koola123@users.noreply.github.com>
2021-05-03 12:20:43 +0200

    Update scripts.js
    Fixed another minor errors in js-file.

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,12 @@ let pokemonList = [
 
 // Loop iterates over each item and prints biggest pokemon to the console
 
-sizing the height of each pokemon and put code into loop for printing
-the biggest pokemon via definition of height */
-
 for (let i=0; i<pokemonList.length; i++) {
     if (pokemonList[i].height <8.8) {
-      document.write("<p>" + pokemonList[i].name + ' (height: ' + pokemonList[i].height +') ' + "</p>");
-      console.log(pokemonList[i].name + " (height: 0.7");
+      document.write("<p>" + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + "</p>");
+      console.log(pokemonList[i]);
     }else {
       document.write("<p>" + pokemonList[i].name + " (height: 8.8)" + " - Wow, that's big!" + "</p>");
-      console.log(pokemonList[i].name + " (height: 8.8)" + " - Wow, that's big!");
+      console.log(pokemonList[i]);
     }
   }
