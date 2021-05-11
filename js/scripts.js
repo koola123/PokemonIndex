3ed3885726d1ce987969727cb1a5b9733297962d
koola123 <82808273+koola123@users.noreply.github.com>
2021-05-11 22:37:23 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,4 @@
 
-// IIFE for Pokemon List
-  (function() {
-    let pokemonList = [
-    {name: "Bulbasaur", height: 0.7, type: ['grass', 'poison']},
-    {name: "Nidoking", height: 1.4, type: ['ground', 'poison']},
-    {name: "Onix", height: 8.8, type: ['rock', 'ground']}
-    ];
-  })();
-
-
 // Pokemon Repository
     let pokemonRepository = (function () {
     let pokemonList = [
@@ -42,7 +32,12 @@
       add: add
     };
   })();
-
-
 pokemonRepository.add({ name:'Charmander', height: 2.0, type: ['fire']});
-pokemonRepository.getAll();
+
+pokemonRepository.getAll().forEach((pokemon) => {
+  if (pokemon.height <8.8) {
+    document.write("<p>" + pokemon.name +  ' (height: ' + pokemon.height + ') ' + "</p>");
+  }else {
+    document.write("<p>" + pokemon.name +  ' (height: ' + pokemon.height + ') ' + " - Wow, that's big!" + "</p>");
+  }
+})
