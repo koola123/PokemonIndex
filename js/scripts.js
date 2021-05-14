4aa2435ee0909b543e26543f732f3a18ae4cbdb9
koola123 <82808273+koola123@users.noreply.github.com>
2021-05-15 01:30:57 +0200

    Update html, css, javascript files
    1. HTML --- adding a new logo and enhanced title.
    2. CSS --- adding a new button class.
    3. JavaScript --- creating list items and a button  to make changes to the DOM for a better app-look.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,69 @@
-
-// Pokemon Repository
-    let pokemonRepository = (function () {
-    let pokemonList = [
+// IIFE Pokemon Repository
+let pokemonRepository = (function () {
+  let pokemonList = [
     {
       name: 'Bulbasaur',
       height: 0.7,
-      type: ['grass', 'poison']
+      type: ['grass', 'poison'],
     },
     {
       name: 'Nidoking',
       height: 1.4,
-      type: ['ground', 'poison']
+      type: ['ground', 'poison'],
     },
     {
       name: 'Onix',
       height: 8.8,
-      type: ['rock', 'ground']
+      type: ['rock', 'ground'],
     }
-    ];
-
-    function getAll() {
-      return pokemonList;
-    }
-
-    function add(pokemon) {
-      pokemonList.push(pokemon);
-    }
-
-    return {
-      getAll: getAll,
-      add: add
-    };
-  })();
-pokemonRepository.add({ name:'Charmander', height: 2.0, type: ['fire']});
-
-pokemonRepository.getAll().forEach((pokemon) => {
-  if (pokemon.height <8.8) {
-    document.write("<p>" + pokemon.name +  ' (height: ' + pokemon.height + ') ' + "</p>");
-  }else {
-    document.write("<p>" + pokemon.name +  ' (height: ' + pokemon.height + ') ' + " - Wow, that's big!" + "</p>");
+  ];
+// function declaration
+  function getAll() {
+    return pokemonList;
   }
-})
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === 'object' &&
+      'name' in pokemon &&
+      'height' in pokemon &&
+      'type' in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log('pokemon is not correct');
+    }
+  }
+// forEach loop function block
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    button.addEventListener('click', function (event) {
+      showDetails(pokemon);
+  });
+
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemonList);
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+  };
+})();
+// function for added new pokemon
+pokemonRepository.add({ name: 'Charmander', height: 2.0, type: ['fire'] });
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
