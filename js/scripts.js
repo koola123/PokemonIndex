9744f2a66b69acbbb37f0284df974fb088f411d8
koola123 <82808273+koola123@users.noreply.github.com>
2021-05-26 01:17:18 +0200

    Revert "update all files"
    This reverts commit d94406b8e7bf44d4788ad9b477810d2e3c5a746b.

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,6 @@ let pokemonRepository = (function () {
     if (
       typeof pokemon === 'object' &&
       'name' in pokemon
-
     ) {
       pokemonList.push(pokemon);
     } else {
@@ -45,7 +44,7 @@ let pokemonRepository = (function () {
           detailsUrl: item.url
         };
         add(pokemon);
-        console.log(pokemon)
+        console.log(pokemon);
       });
     }).catch(function (e) {
       console.error(e);
@@ -67,7 +66,7 @@ let pokemonRepository = (function () {
 // show details function - API
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-
+      console.log(pokemon);
     });
   }
 
@@ -86,60 +85,4 @@ pokemonRepository.loadList().then(function() {
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
 });
-});
-
-// show modal function
-
-function showModal(title, text) {
-  let modalContainer = document.querySelector('#modal-container');
-  modalContainer.innerHTML = '';
-  let modal = document.createElement('div');
-  modal.classList.add('modal');
-
-  let closeButtonElement = document.createElement('button');
-  closeButtonElement.classList.add('modal-close');
-   closeButtonElement.innerText = 'Close';
-  closeButtonElement.addEventListener('click', hideModal);
-
-  let titleElement = document.createElement('h1');
-  titleElement.innerText = title;
-
-  let contentElement = document.createElement('p');
-  contentElement.innerText = text;
-
-  modal.appendChild(closeButtonElement);
-  modal.appendChild(titleElement);
-  modal.appendChild(contentElement);
-  modalContainer.appendChild(modal);
-
-
-
-  modalContainer.classList.add('is-visible');
-}
-
-function hideModal() {
-  let modalContainer = document.querySelector('#modal-container');
-  modalContainer.classList.remove('is-visible');
-}
-
-window.addEventListener('keydown', (e) => {
-  let modalContainer = document.querySelector('#modal-container');
-  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-    hideModal();
-  }
-});
-
-let modalContainer = document.querySelector('#modal-container');
-modalContainer.addEventListener('click', (e) => {
-  // Since this is also triggered when clicking INSIDE the modal
-  // We only want to close if the user clicks directly on the overlay
-  let target = e.target;
-  if (target === modalContainer) {
-    hideModal();
-  }
-});
-
-
-document.querySelector('#show-modal').addEventListener('click', () => {
-  showModal('Pokémon name', 'height');
 });
